801b2fa3123d2f00e7ccc689942f5f1a37360423
Uladzislau Arlouski <uarlouski@gmail.com>
2026-02-18 19:25:21 +0300

    Release 1.3.1

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ if (!TESTRAIL_INSTANCE_URL || !TESTRAIL_USERNAME || !TESTRAIL_API_KEY) {
 
 const server = new McpServer({
     name: "TestRail MCP Server",
-    version: "1.3.0",
+    version: "1.3.1",
 });
 
 const client = new TestRailClient(TESTRAIL_INSTANCE_URL, TESTRAIL_USERNAME, TESTRAIL_API_KEY);
